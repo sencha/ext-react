4671b5ff7b5de1cccbb47eff21c8b50b150dac0e
mgusmano <mgusmano@yahoo.com>
2018-08-20 14:26:21 -0400

    update ext-react-gen

--- packages/ext-react-kitchensink/src/examples/Grid/BasicGrid/BasicGrid.js
+++ packages/ext-react-kitchensink/src/examples/Grid/BasicGrid/BasicGrid.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import { Grid, Column } from '@sencha/ext-modern';
 import model from '../CompanyModel';
+import './data';
 
 export default class BasicGridExample extends Component {
 
   store = Ext.create('Ext.data.Store', {
-    model,
+//    model,
     autoLoad: true,
     pageSize: 0,
     proxy: {
       type: 'ajax',
+
       url: 'resources/data/CompanyData.json'
     } 
+
+  //   data: [{
+  //     "id": 1,
+  //     "company": "abc",
+  //     "name": "Roodel",
+  //     "phone": "602-736-2835",
+  //     "price": 59.47,
+  //     "priceChange": 1.23,
+  //     "priceLastChange": "10/8",
+  //     "industry": "Manufacturing",
+  //     "desc": "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.\n\nCras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+  //     "priceChangePct": 2.11
+  // }]
+
+
+
   });
 
   render() {
