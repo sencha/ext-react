d52bc51759254ad4cf6a7b0b60212f436b4b0435
mgusmano <mgusmano@yahoo.com>
2018-08-30 18:03:58 -0400

    renderwhenready example

--- packages/ext-react-renderwhenready/src/index.js
+++ packages/ext-react-renderwhenready/src/index.js
@@ -4,37 +4,4 @@ import App2 from './App2'
 import { render } from '@sencha/ext-react'
 
 render(<App1/>, document.getElementById('div1'))
-
 render(<App2/>, document.getElementById('div2'))
-
-
-
-
-
-//import { launch } from '@sencha/ext-react'
-//import { ExtReact } from '@sencha/ext-react'
-//launch(() => {return <ExtReact><App/></ExtReact>})
-//launch(<App/>)
-//launch(() => {return <App/>})
-//ReactDOM.render(<App/>, document.getElementById('root'))
-// //console.log(document.getElementById('root'))
-// //ReactDOM.render(<App/>, document.getElementById('root'))
-// ReactDOM.render(<ExtReact><App/></ExtReact>, document.getElementById('root'))
-// let viewport
-
-// const render = (Component, target) => {
-//   ReactDOM.render(<ExtReact><App/></ExtReact>, document.getElementById('root'))
-
-//   // ReactDOM.render(
-//   //   <ExtReact>
-//   //     <AppContainer>
-//   //       <Component/>
-//   //     </AppContainer>
-//   //   </ExtReact>,
-//   //   target
-//   // )
-// }
-
-// launch(target => render(App, viewport = target))
-
-// launch(target => render(App, viewport = target))
