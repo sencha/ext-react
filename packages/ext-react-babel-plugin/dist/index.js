f38f16828dafb2e2f30a1b32e2ccc4836b802cd4
mgusmano <mgusmano@yahoo.com>
2018-09-29 11:12:13 -0400

    add reactVersion

--- packages/ext-react-babel-plugin/dist/index.js
+++ packages/ext-react-babel-plugin/dist/index.js
@@ -8,7 +8,7 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-//var reactVersion = 0 
+var reactVersion = 0;
 var reactVersionFull = '';
 var app = "".concat(_chalk.default.green('ℹ ｢ext｣:'), " ext-react-babel-plugin: ");
 var fs;
