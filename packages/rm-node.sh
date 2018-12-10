81f9105fb9db6d4565a8e968baac1bdd1653406b
mgusmano <mgusmano@yahoo.com>
2018-12-10 13:54:39 -0500

    package.json updates

--- packages/rm-node.sh
+++ packages/rm-node.sh
@@ -1,9 +1,10 @@
 rm -r ext-react/node_modules
 rm -r ext-react-babel-plugin/node_modules
 rm -r ext-react-classic-boilerplate/node_modules
+rm -r ext-react-classic-demo/node_modules
 rm -r ext-react-classic-renderwhenready/node_modules
+rm -r ext-react-classic-renderwhenready-no-plugin/node_modules
 rm -r ext-react-conference-app/node_modules
-rm -r ext-react-demo/node_modules
 rm -r ext-react-froala-example/node_modules
 rm -r ext-react-gen/node_modules
 rm -r ext-react-kitchensink/node_modules
