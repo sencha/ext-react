2b8f5a7c24b4dd2a20e87dbdc89202819b1f1285
Jordan Nelson <jordan.nelson@sencha.com>
2019-04-05 15:28:48 -0700

    Update titles for 7.0

--- packages/ext-react-conference-app/src/about/About.js
+++ packages/ext-react-conference-app/src/about/About.js
@@ -19,7 +19,7 @@ class About extends Component {
         <div style={{padding: Ext.os.is.Phone ? '0 10px': '0 20px'}}>
             <h2 style={{fontWeight: 100}}>ExtReact Conference App</h2>
             <div>
-                This app is built with <a href="https://github.com/sencha/ext-react">Sencha ExtReact 6.7</a> and uses the following libraries:
+                This app is built with <a href="https://github.com/sencha/ext-react">Sencha ExtReact 7.0</a> and uses the following libraries:
                 <ul style={{listStyle: 'none', padding: '0'}}>
                     <li style={listStyle}><a href="https://facebook.github.io/react/">React 16.8.1</a></li>
                     <li style={listStyle}><a href="http://redux.js.org/">React Redux 5.1.1</a></li>
