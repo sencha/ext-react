35ff924d48b586dccd5b8274171a9ff4f1d6e905
Suresh <suresh.chaurasia@celestialsys.com>
2019-07-12 13:03:41 +0530

    Created store to render data in infinite grid also make changes in column as per changes in ext

--- packages/ext-react-kitchensink/src/examples/Grid/InfiniteGrid/InfiniteGrid.js
+++ packages/ext-react-kitchensink/src/examples/Grid/InfiniteGrid/InfiniteGrid.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import { Grid, Column } from '@sencha/ext-modern';
 
+Ext.require([
+    'Ext.grid.filters.Plugin'
+]);
+
 export default class InfiniteGridExample extends Component {
+
+	store = Ext.create('Ext.data.Store', {
+		fields: [
+        	'firstName', 'lastName', 'address', 'company', 'title',
+        {
+            name: 'id',
+            type: 'int'
+        }],
+	    autoLoad: true,
+	    pageSize: 25,
+	    proxy: {
+	      	type: 'ajax',
+	      	url: 'https://llbzr8dkzl.execute-api.us-east-1.amazonaws.com/production/user',
+	      	reader: {
+	            rootProperty: 'users',
+	            totalProperty: 'totalCount'
+	        }
+	    }
+  	})
+
 	render() {
 		return(
 			<Grid
@@ -14,59 +38,42 @@ export default class InfiniteGridExample extends Component {
 			    		gridfilters: true
 			    	}
 			    }
+			    store={this.store}
 			>
 				<Column
-					text="Author"
+					text="First Name"
 			       	width={150}
-			        dataIndex="author"
+			        dataIndex="firstName"
 				>
 				</Column>
 				<Column
-					text="Forum Title"
-			       	width={200}
-			        dataIndex="forum_title"
+					text="Last Name"
+			       	width={150}
+			        dataIndex="lastName"
 				>
 				</Column>
 				<Column
-					text="Forum Id"
-			       	width={80}
-			        dataIndex="forumid"
+					text="Id"
+			       	width={50}
+			        dataIndex="id"
 				>
 				</Column>
 				<Column
-					text="Post Id"
-			       	width={80}
-			        dataIndex="post_id"
+					text="Title"
+			       	flex={1}
+			        dataIndex="title"
 				>
 				</Column>
 				<Column
-					text="Post Text"
-			       	width={200}
-			        dataIndex="post_text"
+					text="Address"
+			       	flex={1}
+			        dataIndex="address"
 				>
 				</Column>
 				<Column
-					text="Post Time"
-			       	width={100}
-			        dataIndex="post_time"
-				>
-				</Column>
-				<Column
-					text="Reply Count"
-			       	width={80}
-			        dataIndex="reply_count"
-				>
-				</Column>
-				<Column
-					text="Topic Id"
-			       	width={100}
-			        dataIndex="topic_id"
-				>
-				</Column>
-				<Column
-					text="Topic Title"
-			       	width={200}
-			        dataIndex="topic_title"
+					text="Company"
+			       	flex={1}
+			        dataIndex="company"
 				>
 				</Column>
 			</Grid>
