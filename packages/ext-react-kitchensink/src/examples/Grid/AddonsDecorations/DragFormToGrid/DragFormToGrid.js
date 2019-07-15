d10bf6a423964138e55a4e3cabb06b448a36198b
Suresh <suresh.chaurasia@celestialsys.com>
2019-07-15 14:58:15 +0530

    Added DragFormToGrid Example in Kitchensink

--- packages/ext-react-kitchensink/src/examples/Grid/AddonsDecorations/DragFormToGrid/DragFormToGrid.js
+++ packages/ext-react-kitchensink/src/examples/Grid/AddonsDecorations/DragFormToGrid/DragFormToGrid.js
@@ -3,8 +3,192 @@ import { Panel, Grid, Column, DataView, Spacer } from '@sencha/ext-modern';
 import { patientData, hospitalData } from './data';
 import './FormToGrid.scss';
 
+Ext.require([
+    'Ext.plugin.dd.DragZone',
+    'Ext.plugin.dd.DropZone'
+]);
 
 export default class DragFormToGridExample extends Component {
+     
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+        this.registerDragZone();
+        this.registerDropZone();
+    }
+
+    registerDragZone = () => {
+        let me = this.refs.dragPanel.cmp;
+        let patientView = this.refs.patientView.cmp;
+        let touchEvents = Ext.supports.Touch && Ext.supports.TouchEvents;
+
+        me.dragZone = Ext.create('Ext.plugin.dd.DragZone', {
+            element: patientView.bodyElement,
+            handle: '.patient-source',
+            view: patientView,  
+            $configStrict: false,
+            activateOnLongPress: touchEvents ? true : false,
+            proxy: {
+                cls: 'x-proxy-drag-el patient-proxy-el'
+            },
+
+            getDragText: function(info) {
+                var selector = '.x-dataview-item',
+                    el = Ext.fly(info.eventTarget).up(selector);
+
+                return el.dom.innerHTML;
+            },
+
+            getDragData: function(e) {
+                return {
+                    patientData: this.view.mapToRecord(e)
+                };
+            }
+        });
+    };
+
+    registerDropZone = () => {
+        let mainContext = this;
+        let me = this.refs.parentPanel.cmp;
+        let hospitalView = this.refs.hospitalView.cmp;
+
+        me.dropZone = Ext.create('Ext.plugin.dd.DropZone', {
+            element: hospitalView.bodyElement,
+            view: hospitalView,
+            $configStrict: false,
+            prepareNameString: me.prepareNameString,
+
+            onDragMove: function(info) {
+                let me = this;
+                let ddManager = Ext.dd.Manager;
+                let targetEl = ddManager.getTargetEl(info);
+                let rowBody = Ext.fly(targetEl);
+                let isRowBody = rowBody.hasCls('hospital-target');
+                let hospital;
+                let patients;
+                let name;
+
+                if (!isRowBody) {
+                    rowBody = Ext.fly(targetEl).parent('.x-rowbody');
+
+                    if (rowBody) {
+                        isRowBody = rowBody.hasCls('hospital-target');
+                    }
+                }
+
+                me.toggleDropMarker(info, false);
+
+                if (!isRowBody) {
+                    return;
+                }
+
+                hospital = rowBody.component.getRecord();
+                patients = hospital.get('patients');
+                name = info.data.dragData.patientData.get('name');
+
+                if (patients && patients.indexOf(name) !== -1) {
+                    return;
+                }
+
+                me.ddEl = rowBody;
+                me.toggleDropMarker(info, true);
+            },
+
+            onDrop: function(info) {
+                let me = this;
+                let hospital;
+                let patients;
+                let name;
+                let component;
+
+                if (!me.ddEl) {
+                    return;
+                }
+
+                component = me.ddEl.component;
+                hospital = component.getRecord();
+                patients = hospital.get('patients');
+                name = info.data.dragData.patientData.get('name');
+
+                if (patients && patients.indexOf(name) !== -1) {
+                    return;
+                }
+
+                if (!patients) {
+                    patients = [];
+                    hospital.set('patients', patients);
+                }
+
+                patients.push(name);
+                component.contentElement.update(mainContext.prepareNameString(patients));
+                me.toggleDropMarker(info, false);
+            },
+
+            toggleDropMarker: function(info, state) {
+                let me = this;
+                let ddManager;
+
+                if (!me.ddEl) {
+                    return;
+                }
+
+                ddManager = Ext.dd.Manager;
+                ddManager.toggleTargetNodeCls(me.ddEl, 'hospital-target-hover', state);
+                ddManager.toggleProxyCls(info, me.validDropCls, state);
+
+                if (!state) {
+                    me.ddEl = null;
+                }
+            }
+        });
+    };
+
+    prepareNameString = (values) => {
+        let str = '';
+        let i = 0;
+        let ln = values.length;
+
+        for (; i < ln; i++) {
+            str += [
+                '<div class="name-tag x-tooltiptool">',
+                '<span>', values[i], '</span>',
+                '<span index="', i,
+                '" class="remove-icon x-icon-el x-font-icon x-tool-type-close"></span></div>'
+            ].join('');
+        }
+
+        return (str || 'Drop patients here');
+    };
+
+    onRemoveTapped = (e, target) => {
+        let mainContext = this;
+        let me = this;
+        let patientIndex = +target.getAttribute('index');
+        let rowBody = Ext.Component.from(target);
+        let record = rowBody.getRecord();
+        let patients = record.get('patients');
+
+        if (patientIndex === -1) {
+            return;
+        }
+
+        patients = Ext.Array.removeAt(patients, patientIndex, 0);
+        rowBody.contentElement.update(mainContext.prepareNameString(patients));
+
+        if (!patients.length) {
+            record.set('patients', null);
+        }
+    };
+
+    destroy = function() {
+        let me = this;
+
+        me.dragZone = me.dropZone = Ext.destroy(me.dragZone, me.dragZone);
+        me.callParent();
+    };
+
 	patientStore = Ext.create('Ext.data.Store', {
         fields: ['name', 'address', 'telephone'],
         data: patientData
@@ -54,17 +238,20 @@ export default class DragFormToGridExample extends Component {
 				cls="form-to-grid-dd"
 				title="Patient Hospital Assignment"
 				maxWidth="800"
+                ref="parentPanel"
 			>
 				<Panel
 					title="Patients"
 			        flex="0.4"
 			        border={true}
 			        scrollable="y"
+                    ref="dragPanel"
 				>
 					<DataView
-						reference="patientView"
+						ref="patientView"
 						itemTpl={this.patientTpl}
 						store={this.patientStore}
+                        onDragStart={this.registerDragZone}
 					>
 					</DataView>
 				</Panel>
@@ -78,7 +265,7 @@ export default class DragFormToGridExample extends Component {
 					flex="0.6"
 			        cls="dd-hospital-grid"
 			        variableHeights={true}
-			        reference="hospitalView"
+			        ref="hospitalView"
 			        itemConfig={
 			        	{
 			        		body: {
@@ -88,6 +275,11 @@ export default class DragFormToGridExample extends Component {
 			        	}
 			        }
 			        store={this.hospitalStore}
+                    listeners={{
+                        element: 'element',
+                        delegate: ['.remove-icon'],
+                        tap: this.onRemoveTapped
+                    }}
 				>
 					<Column
 						dataIndex="name"
