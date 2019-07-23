bf48608eaf0190ae4ed4c884b725eeb105f0a299
Rahul Garg <rgarg@celestialsys.com>
2019-07-23 16:49:35 +0530

    Fixed the issue raised by QA

--- packages/ext-react-kitchensink/src/examples/ToolBars/BreadcrumbToolbar/BreadcrumbToolbar.js
+++ packages/ext-react-kitchensink/src/examples/ToolBars/BreadcrumbToolbar/BreadcrumbToolbar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Panel, BreadcrumbBar, Component as CMP } from '@sencha/ext-modern';
 import fileData from './Files.js'
 
+Ext.require(['Ext.BreadcrumbBar']);
+
 export default class BreadcrumbToolBarExample extends Component {
 	constructor(props) {
 		super(props);
@@ -14,16 +16,15 @@ export default class BreadcrumbToolBarExample extends Component {
         });
 		return (
 			<Panel
-				docked="top"  
-				width="600"
-				height="400"
+				width={600}
+				height={400}
 				overflowHandler="scroller"
-				bodyPadding="20"
+				bodyPadding={20}
 				html="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 			>
 				<BreadcrumbBar 
 					docked="top"
-					showIcons= "true"
+					showIcons= {true}
 					store={this.store}
 				>
 					<CMP 
@@ -39,8 +40,8 @@ export default class BreadcrumbToolBarExample extends Component {
 				</BreadcrumbBar>
 				<BreadcrumbBar 
 					docked="bottom"
-					showIcons= "true"
-					useSplitButtons="false"
+					showIcons= {true}
+					useSplitButtons={false}
 					store={this.store}
 				>
 					<CMP
