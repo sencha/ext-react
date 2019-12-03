b281146ceab7749a796fda5dd2a3f13960a5cb73
mgusmano <mgusmano@yahoo.com>
2019-12-03 07:35:18 -0500

    code window

--- packages/ext-react-kitchensink/src/Files.js
+++ packages/ext-react-kitchensink/src/Files.js
@@ -1,37 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { TabPanel, Panel } from '@sencha/ext-react-modern';
-// import hljs, { highlightBlock } from 'highlightjs';
-// // JSX syntax highlighting
-// import 'highlightjs/styles/atom-one-dark.css';
-import H_js from './H_js';
-// hljs.registerLanguage('js', H_js);
-
-
-import hljs from 'highlight.js/lib/highlight';
-//const hljs = require("highlight.js/lib/highlight");
-hljs.registerLanguage(
-    "javascript",
-    require("highlight.js/lib/languages/javascript")
-);
-hljs.registerLanguage(
-    "typescript",
-    require("highlight.js/lib/languages/typescript")
-);
-hljs.registerLanguage("http", require("highlight.js/lib/languages/http"));
-hljs.registerLanguage("css", require("highlight.js/lib/languages/css"));
-hljs.registerLanguage("xml", require("highlight.js/lib/languages/xml"));
-
+import hljs from 'highlightjs';
+import 'highlightjs/styles/atom-one-dark.css';
 Ext.require('Ext.panel.Resizer');
-
-function codeClassFor(file)  {
-  return 'code'
-  if (file.endsWith('.css')) {
-    return 'css';
-  } else {
-    return 'js xml'
-  }
-}
 
 export default class Files extends Component {
 
@@ -39,58 +11,21 @@ export default class Files extends Component {
     files: PropTypes.object
   }
 
-  // componentWillMount() {
-  //   this._refs = {};
-  // }
-
-  componentDidMount() {
-    //this.highlightCode();
-    //console.log('componentDidMount')
-    this.highlight = true
+  extReactDidMount = detail => {
+    this.highlightCode();
   }
 
   componentDidUpdate(prev) {
     if (this.props.files !== prev.files) {
-      this.hightlight = true
-        //this.highlightCode();
+      this.highlightCode();
     }
   }
 
   highlightCode() {
-    var me = this;
-    // setTimeout(function(){
-    //   alert("Hello");
-    //   //console.log(me.tabs)
-    //   if (me.tabs) for (let el of me.tabs.cmp.el.query('.code')) {
-    //     console.log(el)
-    //     hljs.highlightBlock(el);
-    //   }
-    // }, 3000);
-
-    // console.dir(this.tabs)
-    // if (this.tabs) for (let el of this.tabs.cmp.el.query('.code')) {
-    //   highlightBlock(el);
-    // }
+    document.querySelectorAll("pre code").forEach(block => {
+      hljs.highlightBlock(block);
+    });
   }
-
-  onReady() {
-    //console.log('onReady')
-    //console.log(this.tabs)
-    //console.log(this.highlight)
-    if (this.highlight) {
-      // if (this.tabs) for (let el of this.tabs.cmp.el.query('.code')) {
-      //   console.log(el)
-      //   hljs.highlightBlock(el);
-      // }
-
-      //console.log(document.querySelectorAll("pre code"))
-      document.querySelectorAll("pre code").forEach(block => {
-        //console.log(block)
-        hljs.highlightBlock(block);
-      });
-    }
-  }
-
 
   render() {
     const { files } = this.props;
@@ -98,7 +33,7 @@ export default class Files extends Component {
     return (
       <TabPanel
         ref={(tabs) => {this.tabs = tabs}}
-        onReady={this.onReady.bind(this)}
+        onReady={this.extReactDidMount}
         shadow
         tabBar={{layout: {pack: 'left'}}}
       >
