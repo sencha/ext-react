03eea2ea5c07217f5cb30c3c6eee53ec47c40fbe
mgusmano <mgusmano@yahoo.com>
2020-01-06 15:14:38 -0500

    KS centering

--- packages/ext-react-kitchensink/src/NavView.js
+++ packages/ext-react-kitchensink/src/NavView.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
-import { Container } from '@sencha/ext-react-modern';
+import { Container, Panel } from '@sencha/ext-react-modern';
 
 export default function NavView({ node }) {
+
+    var bodyStyle = `
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    display: flex;
+    alignItems: center;
+    justifyContent: center;
+
+      backgroundSize: 20px 20px;
+      borderWidth: 0px;
+      backgroundColor: #e8e8e8;
+      backgroundImage:
+      linear-gradient(0deg, #f5f5f5 1.1px, transparent 0),
+      linear-gradient(90deg, #f5f5f5 1.1px, transparent 0)
+    `;
+
+
 
   function onClick(e, path) {
     Ext.get(e.target).ripple(e, {});
@@ -10,8 +30,8 @@ export default function NavView({ node }) {
   }
 
   return (
-    <Container layout="center" padding="20" scrollable className="app-navview">
-      <div style={{textAlign: 'center'}}>
+    <Panel layout="center" padding="20" bodyStyle={bodyStyle} scrollable>
+      <div style={{textAlign: 'center', display: 'inline-block'}}>
          { node && node.childNodes.map((child, i) => (
             <div key={i} className="app-thumbnail">
                 <div className="app-thumbnail-icon-wrap" onClick={e => onClick(e, child.id)}>
@@ -22,7 +42,7 @@ export default function NavView({ node }) {
             </div>
         )) }
     </div>
-  </Container>
+  </Panel>
   )
 }
 
