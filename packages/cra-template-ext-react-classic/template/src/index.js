f68cb7da106bef3cdd7a23c42de01da65e00d620
mgusmano <mgusmano@yahoo.com>
2020-01-14 14:39:15 -0500

    cra

--- packages/cra-template-ext-react-classic/template/src/index.js
+++ packages/cra-template-ext-react-classic/template/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import ExtReactDOM from '@sencha/ext-react-classic';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const Ext = window['Ext'];
-Ext.onReady(function () {
-  ReactDOM.render(<App />, document.getElementById('root'));
-});
+ExtReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
