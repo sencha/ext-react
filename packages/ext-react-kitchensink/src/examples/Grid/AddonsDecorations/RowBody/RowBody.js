0fa4aa241615fdab036fcc55f5fc310f1b14c363
mgusmano <mgusmano@yahoo.com>
2020-01-18 08:31:57 -0500

    xtemplate

--- packages/ext-react-kitchensink/src/examples/Grid/AddonsDecorations/RowBody/RowBody.js
+++ packages/ext-react-kitchensink/src/examples/Grid/AddonsDecorations/RowBody/RowBody.js
@@ -95,6 +95,54 @@ var Template = Ext.define(null, {
   }
 });
 
+var getTpl = Ext.XTemplate.getTpl;
+var originalGet = Ext.XTemplate.get;
+
+Ext.XTemplate.get = function (fn) {
+  console.log('h')
+  console.log(typeof fn)
+  console.log(fn)
+  //return new Template(fn);
+  if (typeof fn === 'function') {
+    console.log('hi')
+    return new Template(fn);
+  } else {
+    console.log('else')
+    return originalGet.apply(Ext.XTemplate, arguments);
+  }
+};
+
+Ext.XTemplate.getTpl = function () {
+  console.log('g')
+  return getTpl.apply(Ext.XTemplate, arguments);
+}; // automatically persist event before rippling
+
+
+var originalRipple = Ext.dom.Element.prototype.ripple;
+
+Ext.dom.Element.prototype.ripple = function (event) {
+  if (event && event.persist) event.persist();
+  return originalRipple.apply(this, arguments);
+}; // enable component query by component name in Sencha Test
+
+
+var originalWidgetIsXtype = Ext.Widget.prototype.isXType; // https://github.com/sencha/ext-react/issues/92
+// Ext.Widget.prototype.isXType = function(xtype, shallow) {
+//     return originalWidgetIsXtype.call(this, xtype.toLowerCase().replace(/_/g, '-'), shallow);
+// }
+
+Ext.Widget.prototype.isXType = function (xtype, shallow) {
+  return originalWidgetIsXtype.call(this, xtype.replace(/_/g, '-'), shallow) || originalWidgetIsXtype.call(this, xtype.toLowerCase().replace(/_/g, '-'), shallow);
+}; // needed for classic
+
+
+if (Ext.Component.prototype.isXType) {
+  Ext.Component.prototype.isXType = Ext.Widget.prototype.isXType;
+}
+
+
+
+
 
 
 
