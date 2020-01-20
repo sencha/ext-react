32a7f689c233892ae072916685694fe0e33074a9
mgusmano <mgusmano@yahoo.com>
2020-01-20 07:49:42 -0500

    template

--- demos/ext-react-demos/src/gridrowbody/GridRowBody.js
+++ demos/ext-react-demos/src/gridrowbody/GridRowBody.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {ExtGrid} from "@sencha/ext-react-modern";
-import {ExtColumn, ExtContainer, ExtButton, ExtToolbar} from "@sencha/ext-react-modern";
-import data from './data';
+import {ExtColumn, ExtContainer} from "@sencha/ext-react-modern";
+import data from '../data';
 const Ext = window['Ext'];
 
 export default class GridRowBody extends Component {
@@ -24,62 +24,31 @@ export default class GridRowBody extends Component {
     data
   });
 
-  renderName1 = (value, context) => (
-    <div style={{height:'15px'}}>
-      <span>span - {context.data.name}</span>
-      <ext-button shadow="true" text={context.data.name}></ext-button>
-    </div>
-    //<ExtButton text="hi"></ExtButton>
+  tpl = data => (
+    <span>
+        <div>{data.name}</div>
+        <div>{data.email}</div>
+    </span>
   )
-
-  renderSummaryName1 (value, context) {
-    //console.log(value)
-    //console.log(context)
-    return 'broken'
-    //return context.records.length + ' Names';
-  }
-
-  renderName2 = (value, context) => {
-    //console.log(value)
-    //console.log(context)
-    return value
-  }
-
-  renderSummaryName3 = (value, context) => {
-    //console.log(value)
-    //console.log(context)
-    return context.records.length + ' Names';
-  }
 
   render() {
     return (
 
 <ExtContainer>
 
-    <ExtToolbar>
-      <ExtButton text="Change State" handler={this.changeState}/>
-    </ExtToolbar>
+  <ExtGrid
+    store={this.store}
+    height="400"
+    itemConfig={{
+      body:{
+        tpl: this.tpl
+      }
+    }}
+  >
+    <ExtColumn text="Name" dataIndex="name" flex={2}/>
+  </ExtGrid>
 
-      <ExtGrid
-        store={this.store}
-        plugins= {{gridsummaryrow: true}}
-        itemConfig={{
-          body:{
-              //tpl: this.tpl
-              tpl: '<div>Industry: {industry}</div>' +
-                  '<div>Last Updated: {lastChange:date("Y-m-d g:ia")}</div>' +
-                  '<div style="margin-top: 1em;">{desc}</div>'
-
-          }
-        }}
-      >
-        <ExtColumn text="Name" dataIndex="name" flex={2} renderer={this.renderName1} summaryRenderer={this.renderSummaryName1}/>
-        <ExtColumn text="Name" dataIndex="name" flex={2} renderer={this.renderName2} summary="count"/>
-        <ExtColumn text="Name" dataIndex="name" flex={2} summaryRenderer={this.renderSummaryName3}/>
-        <ExtColumn text="Taken" dataIndex="hoursTaken" flex={1} summary="average"/>
-      </ExtGrid>
-
-      </ExtContainer>
+</ExtContainer>
 
     )
   }
--- packages/ext-react-classic/dist/reactize.js
+++ packages/ext-react-classic/dist/reactize.js
@@ -103,8 +103,13 @@ export default function (CustomElement) {
             var sPropVal = '';
 
             try {
-              sPropVal = JSON.stringify(this.props[prop]);
-              newProps[prop] = sPropVal;
+              sPropVal = JSON.stringify(this.props[prop]); //functions are swallowed - mjg
+
+              if (prop == 'itemConfig') {
+                this.objectProps[prop] = this.props[prop];
+              } else {
+                newProps[prop] = sPropVal;
+              }
             } catch (e) {
               this.objectProps[prop] = this.props[prop];
             }
