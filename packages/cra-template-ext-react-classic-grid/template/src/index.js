e245dc8b0a4fc46d71613d983c47428e7a8e3ca0
mgusmano <mgusmano@yahoo.com>
2020-04-13 15:14:53 -0400

    classic grid template

---
+++ packages/cra-template-ext-react-classic-grid/template/src/index.js
@@ -0,0 +1,15 @@
+import '@sencha/ext-classic-grid-engine';
+import '@sencha/ext-classic-grid-neptune';
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import ExtReactDOM from '@sencha/ext-react-classic-grid';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ExtReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
