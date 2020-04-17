788254711c4de4bced426571470b09960a66945d
mgusmano <mgusmano@yahoo.com>
2020-04-17 11:07:22 -0400

    material-ui

---
+++ packages/ext-react-material-ui/dist/ExtCalendar_month.js
@@ -0,0 +1,3 @@
+import reactize from './common/reactize.js';
+import EWCCalendar_month from '@sencha/ext-web-components-material-ui/dist/ext-calendar-month.component.js';
+export default reactize(EWCCalendar_month);
